3e14fe4017146cd1779e2df13e92e598305a84ec
Roman Shadrin <roman.sha.1998@gmail.com>
2021-04-13 22:50:20 +0300

    My first commit

---
+++ src/Shop.java
@@ -0,0 +1,38 @@
+public class Shop {
+    String name;
+    String city;
+    String street;
+    int houseNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getHouseNumber() {
+        return houseNumber;
+    }
+
+    public void setHouseNumber(int houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+}
